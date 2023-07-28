d4d6b1fa764e9d1d895a8e3732d0e9614744fdb7
Mike Novikov <NovikovM18>
2023-07-28 17:57:34 +0300

    add cstore

--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,20 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    sn: ''
   },
   getters: {
   },
   mutations: {
+    setN(state, payload) {
+      state.sn = payload;
+    }
   },
   actions: {
+    setN(context, payload) {
+      context.commit('setN', payload);
+    }
+
   },
   modules: {
   }
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,44 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>{{ name }}</h1>
+    <h1>{{ storeN }}</h1>
+    <form @submit.prevent="submit">
+      <input type="text" v-model="nn">
+      <button type="submit">set name</button>
+
+    </form>
   </div>
 </template>
+
+<script>
+import store from '@/store';
+
+export default {
+  name: 'About',
+  data() {
+    return {
+      name: 'woohoo',
+      nn: ''
+    }
+  },
+  mounted() {},
+  computed: {
+    storeN() {
+      return this.$store.state.sn;
+    }
+  },
+  methods: {
+    submit() {
+      console.log(store.state)
+      this.$store.dispatch('setN', this.nn);
+      this.nn = '';
+      console.log(store.state)
+
+    }
+  }
+
+}
+</script>
+<style lang="scss">
+  
+</style>
